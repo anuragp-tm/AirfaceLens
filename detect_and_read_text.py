e7e9a2dfc5fb6fff92fbb3b818e0ce0495af006a
anuragp-tm <anurag.pattnaik@tracematrix.in>
2021-10-28 14:21:41 +0530

    OCR text read

--- detect_and_read_text.py
+++ detect_and_read_text.py
@@ -6,31 +6,41 @@ import easyocr
 import os
 from configobj import ConfigObj
 #import configparser
+try:
+    config = ConfigObj('airface_config.ini')
+    imgDir=config['IMAGE_DIR']
+    imgName=config['IMAGE_NAME']
+    imageFile=os.path.join(imgDir,imgName)
+    read_text_method=config['TEXT_READ_METHOD']
+    #text_lang=config['TEXT_LANG']
+    #text_lang=text_lang.split(',')
+    #print(type(text_lang))
+    #print(text_lang)
+    #==== text detection using pytesseract ====== #
+    def read_text_from_image(img):
+        # read an image
+        image=cv2.imread(img)
+        #print(img.shape)
+        # read text from the image
+        text=pytesseract.image_to_string(image)
+        return text
 
-config = ConfigObj('airface_config.ini')
-imgDir=config['IMAGE_DIR']
-imgName=config['IMAGE_NAME']
-imageFile=os.path.join(imgDir,imgName)
-read_text_method=config['TEXT_READ_METHOD']
+    # ===== text detection using easyocr =====#
+    def read_easyocr_text(img):
+        text_lang=config['TEXT_LANG']
+        reader = easyocr.Reader(text_lang)
+        result = reader.readtext(img, detail=0)
+        return result
+    
+    if read_text_method=='pytesseract':
+        textOutput=read_text_from_image(imageFile)
+    elif read_text_method=='easyocr':
+        textOutput='\n' .join(read_easyocr_text(imageFile))
+    #else:
+        #textOutput=''
+        #print("Unavailable option.Please choose either pytesseract or easyocr ")
 
-
-#==== text detection using pytesseract ====== #
-def read_text_from_image(img):
-    # read an image
-    image=cv2.imread(img)
-    #print(img.shape)
-    # read text from the image
-    text=pytesseract.image_to_string(image)
-    return text
-
-# ===== text detection using easyocr =====#
-def read_easyocr_text(img):
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(img, detail=0)
-    return result
-
-text=read_text_from_image(imageFile)
-result=read_easyocr_text(imageFile)
-
-textOutput=text if read_text_method=="pytesseract" else ' '.join(result)
-print(textOutput)
+    #textOutput=text if read_text_method=="pytesseract" else ' '.join(result)
+    print(textOutput)
+except Exception as e:
+    print(f"Failed to read text from the image.{e}")
--- generate_pdf_from_text.py
+++ generate_pdf_from_text.py
@@ -1,40 +1,44 @@
 from fpdf import FPDF
 import os
 from configobj import ConfigObj
-
-config = ConfigObj('airface_config.ini')
-
 from detect_and_read_text import textOutput
-
-result=textOutput
-imgName=config['IMAGE_NAME'].split(".")[0]
-target_text_dir=config['TARGET_TXT_FILE_DIR']
-target_text_file_path=os.path.join(target_text_dir, f"{imgName}.txt")
-target_pdf_dir=config['TARGET_PDF_FILE_DIR']
-target_pdf_file_path=os.path.join(target_pdf_dir, f"{imgName}.pdf")
-with open(target_text_file_path, "w+") as f:
-        f.write(result)
+try:
+        config = ConfigObj('airface_config.ini')
 
 
-# save FPDF() class into
-# a variable pdf
-pdf = FPDF()
-
-# Add a page
-pdf.add_page()
-
-# set style and size of font
-# that you want in the pdf
-pdf.set_font("Arial", size=15)
+        result=textOutput
+        imgName=config['IMAGE_NAME'].split(".")[0]
+        target_text_dir=config['TARGET_TXT_FILE_DIR']
+        target_text_file_path=os.path.join(target_text_dir, f"{imgName}.txt")
+        target_pdf_dir=config['TARGET_PDF_FILE_DIR']
+        target_pdf_file_path=os.path.join(target_pdf_dir, f"{imgName}.pdf")
+        with open(target_text_file_path, "w+") as f:
+                f.write(result)
 
 
+        # save FPDF() class into
+        # a variable pdf
+        pdf = FPDF()
+
+        # Add a page
+        pdf.add_page()
+
+        # set style and size of font
+        # that you want in the pdf
+        pdf.set_font("Arial", size=15)
 
 
-# insert the texts in pdf
-with open(target_text_file_path, "r") as f:
- for x in f:
-    pdf.cell(200, 10, txt=x, ln=1, align='C')
 
-# save the pdf with name .pdf
-pdf.output(target_pdf_file_path)
-f.close()
+
+        # insert the texts in pdf
+        with open(target_text_file_path, "r") as f:
+           for x in f:
+                pdf.cell(200, 10, txt=x, ln=1, align='C')
+
+        # save the pdf 
+        pdf.output(target_pdf_file_path)
+        print("PDF file created from text.")
+        f.close()
+except Exception as e :
+  print("Failed to created PDF.{e}")
+  
